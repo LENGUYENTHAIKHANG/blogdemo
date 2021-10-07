175b28033451274af938f77d9fbcaca0e2781ce3
LENGUYENTHAIKHANG <80744289+LENGUYENTHAIKHANG@users.noreply.github.com>
2021-10-07 10:11:46 +0700

    3

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './Compornents/Header/Header';
 
 
 import Page from './Compornents/Page';
-import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import SinglePost from './Compornents/singlePost/SinglePost';
 import Write from './Compornents/Write/Write';
 import Acc from './Compornents/Acc/Acc';
@@ -13,22 +13,34 @@ import Login from './Compornents/Login/Login';
 import Register from './Compornents/Login/Register';
 
 function App() {
-  return (<Router>
+  const user=false;
+
+  return (
+  <Router>
     <div className="app">
-      
+
+
+      <Topbar />
+      <Switch>
+        <Route exact path="/" component={Page} >
+          {user ?<Page/>:<Login/>}
+        </Route>
+        <Route exact path="/sp/:id" component={SinglePost} >
+        {user ?<SinglePost/>:<Login/>}
+        </Route>
+        <Route exact path="/write" component={Write} >
+        {user ?<Write/>:<Login/>}
+        </Route>
+        <Route exact path="/acc" component={Acc} >
+        {user ?<Acc/>:<Login/>}
+        </Route>
+
+        <Route exact path="/register" component={Register} />
         
-      <Topbar/>
-<Switch>
-      <Route  path="/" component={Page} />
-      <Route  path="/sp/:id" component={SinglePost} />
-      <Route  path="/write" component={Write} />
-      <Route  path="/acc" component={Acc} />
-      
-      <Route  path="/register" component={Register} />
-      <Route exact path="/login" component={Login} />
-       </Switch>
-   </div>
-   </Router>
+        <Route exact path="/login" component={Login} />
+      </Switch>
+    </div>
+  </Router>
   );
 }
 
